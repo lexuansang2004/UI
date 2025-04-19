2bbcf7ec3cf4e400697400e8135854eb0cfacbc5
lexuansang2004 <lexuansang14042004@gmail.com>
2025-04-19 13:00:08 +0700

    Update Tuan6

---
+++ Tuan6/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductListingPage from './components/ProductListingPage'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 via-white to-gray-100">
+      <ProductListingPage />
+    </div>
+  )
+}
+
+export default App
